3581d7f2beeceb04d6052596007b6b204d4c4372
pengqiang <pengqiang.x@gmail.com>
2017-07-10 15:22:15 +0800

    彭强：base code

---
+++ javaCore/src/main/java/com/github/pengqiangx/demo/javacore/base/HasStatic.java
@@ -0,0 +1,18 @@
+package com.github.pengqiangx.demo.javacore.base;
+
+/**
+ * Created by pengqiang on 2017/7/10.
+ */
+public class HasStatic {
+    private static int x=100;
+    public static  void  main(String  args[]){
+        HasStatic hs1 = new HasStatic();
+        hs1.x++;
+        HasStatic  hs2=new  HasStatic(  );
+        hs2.x++;
+        hs1=new  HasStatic( );
+        hs1.x++;
+        HasStatic.x--;
+        System.out .println("x="+x);
+    }
+}
