a694d93555d3652d67deac1bd2c5302482d59b2d
pengqiang <pengqiang.x@gmail.com>
2017-08-15 16:24:53 +0800

    彭强：test

---
+++ javaCore/src/main/java/com/github/pengqiangx/demo/javacore/match/TestOne.java
@@ -0,0 +1,13 @@
+package com.github.pengqiangx.demo.javacore.match;
+
+import org.junit.Test;
+
+/**
+ * Created by pengqiang on 2017/6/12.
+ */
+public class TestOne {
+    @Test
+    public void test1(){
+        System.out.print("10,20".indexOf("45"));
+    }
+}
