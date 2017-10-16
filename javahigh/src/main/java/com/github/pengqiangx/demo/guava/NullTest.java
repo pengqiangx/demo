ebd62672e501015edc362582f5e557634e87dc45
pengqiang <pengqiang.x@gmail.com>
2017-10-16 14:31:48 +0800

    test alibaba check style

---
+++ javahigh/src/main/java/com/github/pengqiangx/demo/guava/NullTest.java
@@ -0,0 +1,14 @@
+package com.github.pengqiangx.demo.guava;
+
+import org.junit.Test;
+
+/**
+ * Created by pengqiang on 2017/10/15.
+ */
+public class NullTest {
+    @Test
+    public void def(){
+        byte c;
+        System.out.println("");
+    }
+}
---
+++ javahigh/src/main/java/com/github/pengqiangx/demo/guava/OptionalTest.java
@@ -0,0 +1,8 @@
+package com.github.pengqiangx.demo.guava;
+
+/**
+ * Created by pengqiang on 2017/10/14.
+ */
+public class OptionalTest {
+
+}
