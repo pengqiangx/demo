0d5346c8e685111a91a68bda82d1c19019871bcc
pengqiang <pengqiang.x@gmail.com>
2017-12-11 17:32:08 +0800

    test log

---
+++ eureka-feign/src/main/java/com/ai/cloud/service/ComputeClient.java
@@ -0,0 +1,18 @@
+package com.ai.cloud.service;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author xiaoniu 2017/12/11.
+ */
+
+
+@FeignClient("compute-service")
+public interface ComputeClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/add")
+    String add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
+
+}
